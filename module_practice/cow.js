86c9f60d6e3ed48650044ede3a730d7c37cfef1a
JonathanStewart2 <Jonathan.Stewart2@hotmail.com>
2022-10-05 16:21:58 +0100

    updating

--- module_practice/cow.js
+++ module_practice/cow.js
@@ -3,9 +3,18 @@
 const cows = require('cowsay');
 
 const whatDoesTheCowSay = (string) => console.log(cows.say({'text': string})); 
+const wholeCow = (say, eye, tongue) => {
+    console.log(cows.say(
+        {text: say,
+        e : eye,
+        T : tongue
+        })); 
+}
+
 
 //console.log(whatDoesTheCowSay("moo"));
 
 module.exports = {
-    whatDoesTheCowSay
+    whatDoesTheCowSay,
+    wholeCow
 };
--- module_practice/index.js
+++ module_practice/index.js
@@ -1,5 +1,8 @@
 const cow = require("./cow.js");
 
 
-cow.whatDoesTheCowSay("moo");
-cow.whatDoesTheCowSay("Honk Honk, I'm a clown!");
+// cow.whatDoesTheCowSay("moo");
+// cow.whatDoesTheCowSay("Honk Honk, I'm a clown!");
+// cow.cowSayEye("Moo", "pp");
+
+cow.wholeCow("HONK HONK, I'm a clown!", "xo", "ll");
